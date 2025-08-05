b0781d4605748d924c1a2c73dbb0e4f4e225153e
dikaheni89 <digitalsoftware957@gmail.com>
2025-08-05 17:03:08 +0700

    feat: update Backend rust modification backend

--- src/application/api/user_controller.rs
+++ src/application/api/user_controller.rs
@@ -1,5 +1,12 @@
-use actix_web::{post, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
 use crate::business::usecase::create_user::CreateUserUseCase;
+use crate::business::services::user_service::UserService; // Tambahkan jika perlu
+
+#[get("/users")]
+pub async fn get_users() -> impl Responder {
+    let users = UserService::list_users();
+    HttpResponse::Ok().json(users)
+}
 
 #[post("/users")]
 pub async fn create_user(form: web::Json<(String, String)>) -> impl Responder {
