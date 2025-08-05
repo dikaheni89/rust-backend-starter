89f6f16efb16e78578c844196b976fd9597315d6
dikaheni89 <digitalsoftware957@gmail.com>
2025-08-05 16:20:12 +0700

    feat: update Library Entry Point libs

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! Library Entry Point
+//!
+//! Re-export all modules needed for external use.
+//! This makes your backend modular and reusable as a library.
+
+pub mod application;
+pub mod business;
+pub mod infrastructure;
+pub mod core;
+pub mod external;
